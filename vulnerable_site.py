ed7f64cd932b2c9c937ef804febdcb1c6e5503c2
Josiah <mcflyhalf@live.com>
2019-07-17 09:06:21 +0400

    Most functionality present. Now only needs DB schema to test with the DB and include working SQL queries in the code.

--- vulnerable_site.py
+++ vulnerable_site.py
@@ -1,21 +1,27 @@
 #Get the Flask app
-
+from flask import Flask
 #Example code that I will use to develop the functionality
-import MySQLdb #This module is currently not included in reqs.txt
+import sqlite3 #This module is currently not included in reqs.txt
 
-host = '192.168.99.100'
-user = 'myuser'
-password = 'secret'
-port = 3306
-db = 'test'
+vuln_app = Flask(__name__)
 
-conn = MySQLdb.Connection(
-    host=host,
-    user=user,
-    passwd=password,
-    port=port,
-    db=db
-)
+
+# host = '127.0.0.1'
+# user = 'myuser'
+# password = 'secret'
+# port = 3306
+db = 'vuln_shop.db'
+
+# conn = sqlite3.Connect(
+#     host=host,
+#     user=user,
+#     passwd=password,
+#     port=port,
+#     db=db
+# )
+
+conn = sqlite3.Connect(db)
+cur = conn.cursor()
 
 
 
@@ -23,8 +29,8 @@ isAuthenticated = False
 # Example of how to insert new values:
 #conn.query("""INSERT INTO mytable VALUES ('foo3', 'bar2')""")
 def authenticate(user, passwd,conn=conn):
-	conn.query("""SELECT ?,? FROM ? WHERE ?=\'user\' AND ?=\'passwd\'""")
-	conn.commit()
+	cur.execute("""SELECT ?,? FROM ? WHERE ?=\'user\' AND ?=\'passwd\'""")
+	res = cur.fetchone()
 
 	result = {'ValidUser':False, 'ValidPassword':False}
 	if something:
@@ -52,12 +58,49 @@ conn.commit()
 
 #Form that allows you to search and see prices of things
 #/shop
+@app.route('/',methods=['GET'],['POST'])
+def shop():
+	if request.method == 'GET':
+		return render_template('search.html',productList=[])
+
+	else:
+		#TODO: Ensure username and password are not empty first
+
+
+		cur.execute("""SELECT 'name','description','price' FROM ? WHERE 'name' LIKE *TODO*""" )
 
 
 
-#Tablle that allows you to set prices of things (Should also have a message to display for hints)
+
+#Table that allows you to set prices of things (Should also have a message to display for hints)
 #/admin
 
 
 
 #/login
+@app.route('/login',methods= ['GET','POST'])
+def login():
+	if request.method == 'POST':
+		res = authenticate(username,password)
+		msg = ''
+		if res['ValidUser']:
+			msg+= 'Valid Username'
+		else:
+			msg+= 'Invalid Username'
+
+		if res['ValidPassword']:
+			msg+= ' and Valid Password'
+		else:
+			msg+= ' and Invalid Password'
+
+		if not (res['ValidUser'] and res['ValidPassword']):
+			return render_template('login.html', message=msg)
+
+		return url_for(success)
+
+
+
+	else:
+		'''Show the login form (for a get request)'''
+		return render_template('login.html', message='')
+
