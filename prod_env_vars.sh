38d76f94d800d0741cca2b87d9ce6e083b5e7351
Ubuntu <ubuntu@ip-172-31-16-221.ec2.internal>
2019-07-18 18:00:34 +0000

    pyc file

---
+++ prod_env_vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export vuln_db=sales
+export SQL_USER=Lina
+export SQL_PASS=GDGmysqli
+export SQL_PORT=3306
+export FLASK_APP=vulnerable_site.py
+export FLASK_ENV=production
+#export FLASK_ENV=development
+
+
+
