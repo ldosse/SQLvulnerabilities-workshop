aca6f5b214ed38c8772e5f24c09f02ae50de6f75
Ubuntu <ubuntu@ip-172-31-16-221.ec2.internal>
2019-07-18 12:08:54 +0000

    Server side commit that I shouldnt have done

--- web_req.py
+++ web_req.py
@@ -10,7 +10,7 @@ login_data['username']= 'testuser'
 login_data['password']= 'testpassword'
 
 resp1 = requests.get(index_url, params=None)
-resp2 = requests.post(index_url, data=indexdata)
+resp2 = requests.post(index_url, data=index_data)
 resp3 = requests.get(login_url, params=None)
 resp4 = requests.post(login_url, data=login_data)
 
