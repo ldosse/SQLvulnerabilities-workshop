d1e8dea0a5973ff2635009da512502fcb1c7e5f5
Morema <25913934+Mo-rema07@users.noreply.github.com>
2019-07-18 22:24:56 +0400

    test buy page

--- web_req.py
+++ web_req.py
@@ -5,10 +5,11 @@ index_data = dict()
 index_data['search'] = 'jea'
 
 login_url = 'http://localhost:5000/login'
+buy = 'http://localhost:5000/buy'
 login_data = {'username': 'testuser', 'password': 'testpassword'}
 
 resp1 = requests.get(index_url, params=None)
 resp2 = requests.post(index_url, data=index_data)
 resp3 = requests.get(login_url, params=None)
 resp4 = requests.post(login_url, data=login_data)
-
+resp5 = requests.post(buy, params=None)
