472cb9862dd2136ceb1333331209c62bea12a121
Josiah <mcflyhalf@live.com>
2019-07-18 15:52:00 +0400

    Include tests for login route in webreq

--- web_req.py
+++ web_req.py
@@ -1,10 +1,15 @@
 import requests
 
-url =  ''
-data = dict{}
+index_url =  'http://localhost/'
+index_data = dict()
+index_data['search'] = 'jea'
 
-data['search'] = 'jea'
+login_url = 'http://localhost/login'
+login_data= {}
+login_data['username']= 'testuser'
+login_data['password']= 'testpassword'
 
-
-resp1 = requests.get(url, params=None)
-resp2 = requests.post(url, data=data)
+resp1 = requests.get(index_url, params=None)
+resp2 = requests.post(index_url, data=indexdata)
+resp3 = requests.get(login_url, params=None)
+resp4 = requests.post(login_url, data=login_data)
