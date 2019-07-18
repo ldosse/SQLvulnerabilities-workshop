3c5938e58ebdbe8382e7daeb86be64da66bf632a
Morema <25913934+Mo-rema07@users.noreply.github.com>
2019-07-18 21:58:00 +0400

    Added a page to confirm a sale

--- web_req.py
+++ web_req.py
@@ -1,15 +1,13 @@
 import requests
 
-index_url =  'http://localhost:5000/'
+index_url = 'http://localhost:5000/'
 index_data = dict()
 index_data['search'] = 'jea'
 
 login_url = 'http://localhost:5000/login'
-login_data= {}
-login_data['username']= 'testuser'
-login_data['password']= 'testpassword'
+login_data = {'username': 'testuser', 'password': 'testpassword'}
 
 resp1 = requests.get(index_url, params=None)
-resp2 = requests.post(index_url, data=indexdata)
+resp2 = requests.post(index_url, data=index_data)
 resp3 = requests.get(login_url, params=None)
 resp4 = requests.post(login_url, data=login_data)
